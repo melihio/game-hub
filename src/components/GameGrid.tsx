094032a94b2e1071beb3c366be7949a4b9ea35e2
melihio <melihcokan@icloud.com>
2023-10-10 21:05:28 +0300

    Building game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Spinner, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -8,11 +9,15 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       {isLoading && <Spinner size="xl" />}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding={10}
+        spacing="10px"
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game}></GameCard>
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
