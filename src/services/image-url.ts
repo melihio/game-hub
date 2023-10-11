e8ff309dfddf3e4e50a95d57752492f2ac5ba22e
melihio <melihcokan@icloud.com>
2023-10-11 18:18:09 +0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,8 @@
+import placeHolder from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return placeHolder;
+
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
